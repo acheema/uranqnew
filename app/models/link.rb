271a919d7d63be318d16b5a70b13aeae86b1339c
akaljot <akaljot.cheema@gmail.com>
2014-10-09 11:15:19 -0700

    Added votes model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ class Link < ActiveRecord::Base
 	#attr_accessor :url, :title
 	belongs_to :user
 	has_many :comments
+	has_many :votes
 
 	def self.createNewLink(linktitle, linkurl)
 		newLink = Link.new(title: linktitle, url: linkurl)
