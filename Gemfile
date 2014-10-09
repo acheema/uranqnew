947da71566e8531b73fee796749c539b35fdf214
akaljot <akaljot.cheema@gmail.com>
2014-10-08 17:45:18 -0700

    Added pagination

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer',  platforms: :ruby
 
 gem 'devise'
+gem 'will_paginate', '~> 3.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
 	def index
-		@links = Link.all
+		params[:per_page] ||= 15
+		params[:page]     ||= 1
+		@links = Link.all.paginate(page: params[:page], per_page: params[:per_page])
 	end
 end
