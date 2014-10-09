f8c2d025dfd4f9db2cc37c7fdc6c1c87f89c75eb
akaljot <akaljot.cheema@gmail.com>
2014-10-08 21:44:49 -0700

    Added comments

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ActiveRecord::Base
 	#attr_accessor :url, :title
 	belongs_to :user
+	has_many :comments
 
 	def self.createNewLink(linktitle, linkurl)
 		newLink = Link.new(title: linktitle, url: linkurl)
