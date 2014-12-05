ca5e3d36f88e7749967559f2db9f3c62d32074e3
Akaljot Cheema <akaljot.cheema@gmail.com>
2014-12-04 20:59:46 -0800

    Fixes and improvements for show listings

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,8 +4,8 @@ class Link < ActiveRecord::Base
 	has_many :comments
 	has_many :votes
 
-	def self.createNewLink(linktitle, linkurl, linkid)
-		newLink = Link.new(title: linktitle, url: linkurl, user_id: linkid)
+	def self.createNewLink(linktitle, linkurl, descrip, linkid)
+		newLink = Link.new(title: linktitle, url: linkurl, user_id: linkid, description: descrip)
 		newLink.save!
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009180318) do
+ActiveRecord::Schema.define(version: 20141205035915) do
 
   create_table "comments", force: true do |t|
     t.text     "message"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141009180318) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.string   "description"
   end
 
   create_table "users", force: true do |t|
