0b359c8fd9dcccbb01c6045801cbb65a0358f2ef
Akaljot Cheema <akaljot.cheema@gmail.com>
2014-12-04 21:29:32 -0800

    Added position at company to DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit :username, :email, :password, :password_confirmation
+        u.permit :username, :position, :email, :password, :password_confirmation
       end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205050849) do
+ActiveRecord::Schema.define(version: 20141205051836) do
 
   create_table "comments", force: true do |t|
     t.text     "message"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141205050849) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "position"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
