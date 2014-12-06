969de066b5f94b5c5e162cb44dad7feba4f1674f
lee-yuanming <ylee010@berkeley.edu>
2014-12-06 10:43:09 +0000

    major design overhaul

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
 	def index
-		params[:per_page] ||= 25
+		params[:per_page] ||= 5
 		params[:page]     ||= 1
 		@links = Link.all.paginate(page: params[:page], per_page: params[:per_page])
 	end
